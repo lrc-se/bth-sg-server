6942be8e0d2a79a8907902dc43ed6c659b688392
Kalle Brisland <kabc16@student.bth.se>
2017-11-17 20:47:02 +0100

    Added base for wordlist module

---
+++ services/wordlist.js
@@ -0,0 +1,24 @@
+/**
+ * Wordlist module.
+ *
+ * @module  services/wordlist
+ */
+
+"use strict";
+
+var words;
+
+
+const Wordlist = {
+    /**
+     * Returns the current wordlist.
+     *
+     * @returns {Array}     The wordlist.
+     */
+    getWords: function() {
+        return words;
+    }
+};
+
+
+module.exports = Wordlist;
