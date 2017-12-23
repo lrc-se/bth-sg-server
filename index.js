46df64757762ddee12409f309d3f3439b541fb0a
Kalle Brisland <kabc16@student.bth.se>
2017-12-23 23:54:15 +0100

    Changed server structure to use Express singelton

--- index.js
+++ index.js
@@ -9,12 +9,13 @@
 const fs = require("fs");
 const path = require("path");
 const http = require("http");
-const SgApp = require("./src/sg-app");
 const SgGame = require("./src/sg-game");
 
 
 // default config
 let config = {
+    port: 1700,
+    pingTimeout: 30000,
     games: [
         {
             port: 1701,
@@ -26,9 +27,6 @@ let config = {
         }
     ]
 };
-
-// game servers
-let games = [];
 
 
 // load config
@@ -51,40 +49,53 @@ fs.readFile(path.join(__dirname, "./config.json"), "utf8", function(err, data) {
         console.log("No config file found; using defaults");
     }
     
-    // create game servers
-    for (let num = 1; num <= config.games.length; ++num) {
-        let cfg = config.games[num - 1];
-        
-        // check wordlist
-        cfg.wordlist = path.join(__dirname, cfg.wordlist);
-        if (!fs.existsSync(cfg.wordlist)) {
-            console.error(`Could not find wordlist for game server #${num}:`, cfg.wordlist);
-            continue;
+    // create and start main server
+    const app = require("./src/sg-app");
+    const server = http.createServer(app);
+    server.listen(config.port, function(err) {
+        if (err) {
+            console.error(`Error starting main server:`, err);
+            return;
         }
+        console.log(`Main server running on port ${config.port}`);
         
-        // setup
-        let server = http.createServer(SgApp());
-        let game = SgGame({
-            httpServer: server,
-            pingTimeout: config.pingTimeout
-        }, cfg);
-        let gameServer = {
-            server: server,
-            game: game,
-            num: num
-        };
-        games.push(gameServer);
-        
-        // start server
-        server.listen(cfg.port, function(err) {
-            if (err) {
-                console.error(`Error starting game server #${gameServer.num}:`, err);
-                return;
+        // create game servers
+        for (let num = 1; num <= config.games.length; ++num) {
+            let cfg = config.games[num - 1];
+            
+            // check wordlist
+            cfg.wordlist = path.join(__dirname, cfg.wordlist);
+            if (!fs.existsSync(cfg.wordlist)) {
+                console.error(`Could not find wordlist for game server #${num}:`, cfg.wordlist);
+                continue;
             }
-            console.log(
-                `Game server #${gameServer.num} running on port ${cfg.port} ` +
-                `(min: ${cfg.minPlayers}, max: ${cfg.maxPlayers}, timeout: ${cfg.timeout})`
-            );
-        });
-    }
+            
+            // setup
+            let server2 = http.createServer();
+            let game = SgGame({
+                httpServer: server2,
+                pingTimeout: config.pingTimeout
+            }, cfg);
+            let gameServer = {
+                name: cfg.name,
+                server: server2,
+                port: cfg.port,
+                game: game,
+                num: num
+            };
+            app.games.push(gameServer);
+            
+            // start server
+            server2.listen(cfg.port, function(err) {
+                if (err) {
+                    console.error(`Error starting game server #${gameServer.num}:`, err);
+                    return;
+                }
+                console.log(
+                    `Game server #${gameServer.num} running on port ${cfg.port} ` +
+                    `(min: ${cfg.minPlayers}, max: ${cfg.maxPlayers}, timeout: ${cfg.timeout})`
+                );
+            });
+        }
+    });
 });
--- src/sg-app.js
+++ src/sg-app.js
@@ -9,24 +9,16 @@
 const express = require("express");
 
 
-/**
- * Creates a new S&G app instance.
- *
- * @returns {object}    Express app instance.
- */
-function createApp() {
-    // init
-    let app = express();
-    
-    // set up 404
-    app.use(function(req, res, next) {
-        let err = new Error("404 Not Found");
-        err.status = 404;
-        next(err);
-    });
-    
-    return app;
-}
+// init
+let app = express();
+app.games = [];
+
+// set up 404
+app.use(function(req, res, next) {
+    let err = new Error("404 Not Found");
+    err.status = 404;
+    next(err);
+});
 
 
-module.exports = createApp;
+module.exports = app;
